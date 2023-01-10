b58e174a939dd7c4c36b87de4373432e337c5c3d
mwxgh <mwx6996@gmail.com>
2023-01-10 10:32:03 +0700

    feat: update auth module

--- src/components/auth/controllers/auth.controller.ts
+++ src/components/auth/controllers/auth.controller.ts
@@ -1,20 +1,14 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { JwtService } from '@nestjs/jwt'
+import { Body, Controller, Post } from '@nestjs/common'
 import {
   ApiHeader,
   ApiOkResponse,
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger'
-import { UserService } from '@userModule/services/user.service'
-import { OAuth2Client } from 'google-auth-library'
-import { isNil, pick } from 'lodash'
 import { LoginGoogleDto, UserLoginDto, UserRegisterDto } from '../dto/auth.dto'
 import { AuthService } from '@authModule/services/auth.service'
 import { AttributeAuthentication } from '@authModule/interfaces/auth.interface'
 
-const authenticatedUserFields = ['id', 'email']
 @ApiTags('Auth')
 @ApiHeader({
   name: 'Content-Type',
@@ -22,59 +16,15 @@ const authenticatedUserFields = ['id', 'email']
 })
 @Controller('api/auth')
 export class AuthController {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-    private config: ConfigService,
-    private authService: AuthService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Post('loginGoogle')
   @ApiOperation({ summary: 'Login with google token' })
   @ApiOkResponse({ description: 'Token for access system' })
   async googleAuthCallback(
-    @Body() body: LoginGoogleDto,
+    @Body() data: LoginGoogleDto,
   ): Promise<AttributeAuthentication> {
-    const client = new OAuth2Client(this.config.get('GOOGLE_CONSUMER_KEY'))
-
-    let ticket
-
-    try {
-      ticket = await client.verifyIdToken({
-        idToken: body.idToken,
-      })
-    } catch (e) {
-      throw new BadRequestException('Token is not valid')
-    }
-
-    const payload = ticket.getPayload()
-
-    if (!payload) {
-      throw new BadRequestException('Can not parser idToken')
-    }
-
-    const email: string = payload.email
-
-    if (isNil(email) || email === '') {
-      throw new BadRequestException('Can not get email address')
-    }
-
-    let user = await this.userService.first({ where: { email } })
-
-    if (!user) {
-      user = await this.userService.create({
-        email: this.userService.sanitizeEmail(email),
-        firstName: payload.givenName,
-        lastName: payload.familyName,
-      })
-    }
-
-    const resource = {
-      token: this.jwtService.sign(pick(user, authenticatedUserFields)),
-      expiresIn: process.env.JWT_TTL,
-    }
-
-    return resource
+    return this.authService.googleLogin(data)
   }
 
   @Post('/register')
@@ -84,7 +34,7 @@ export class AuthController {
     @Body() data: UserRegisterDto,
   ): Promise<AttributeAuthentication> {
     // check user exist
-    return await this.authService.register(data)
+    return this.authService.register(data)
   }
 
   @Post('/login')
--- src/components/auth/services/auth.service.ts
+++ src/components/auth/services/auth.service.ts
@@ -1,11 +1,17 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { BaseService } from '@sharedServices/base.service'
 import { UserService } from '@userModule/services/user.service'
-import { UserLoginDto, UserRegisterDto } from '../dto/auth.dto'
-import { pick } from 'lodash'
+import { LoginGoogleDto, UserLoginDto, UserRegisterDto } from '../dto/auth.dto'
+import { isNil, pick } from 'lodash'
 import { JwtService } from '@nestjs/jwt'
 import { UserEntity } from '@userModule/entities/user.entity'
 import { AttributeAuthentication } from '@authModule/interfaces/auth.interface'
+import { OAuth2Client } from 'google-auth-library'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService extends BaseService {
@@ -13,10 +19,16 @@ export class AuthService extends BaseService {
   constructor(
     private userService: UserService,
     private jwtService: JwtService,
+    private config: ConfigService,
   ) {
     super()
   }
 
+  /**
+   * Generate token
+   * @param params Partial UserEntity
+   * @returns AttributeAuthentication
+   */
   private _generateToken(params: Partial<UserEntity>): AttributeAuthentication {
     const token = this.jwtService.sign(params)
     return {
@@ -25,6 +37,11 @@ export class AuthService extends BaseService {
     }
   }
 
+  /**
+   * Register native user
+   * @param params UserRegisterDto
+   * @returns AttributeAuthentication
+   */
   async register(params: UserRegisterDto): Promise<AttributeAuthentication> {
     const user = await this.userService.saveUser({
       data: params,
@@ -38,6 +55,11 @@ export class AuthService extends BaseService {
     return this._generateToken(partialUserProperties)
   }
 
+  /**
+   * Login native user
+   * @param params UserLoginDto
+   * @returns AttributeAuthentication
+   */
   async login(params: UserLoginDto): Promise<AttributeAuthentication> {
     const { email, password } = params
 
@@ -56,6 +78,54 @@ export class AuthService extends BaseService {
     if (isValidPassword != true) {
       throw new UnauthorizedException('Password does not match')
     }
+
+    const partialUserProperties: Partial<UserEntity> = pick(
+      user,
+      this.authenticatedUserFields,
+    )
+
+    return this._generateToken(partialUserProperties)
+  }
+
+  /**
+   * Login via google
+   * @param params UserLoginDto
+   * @returns AttributeAuthentication
+   */
+  async googleLogin(params: LoginGoogleDto) {
+    const { idToken } = params
+    const client = new OAuth2Client(this.config.get('GOOGLE_CONSUMER_KEY'))
+
+    let ticket
+
+    try {
+      ticket = await client.verifyIdToken({
+        idToken,
+      })
+    } catch (e) {
+      throw new BadRequestException('Token is not valid')
+    }
+
+    const payload = ticket.getPayload()
+
+    if (!payload) {
+      throw new BadRequestException('Can not parser idToken')
+    }
+
+    const { email, givenName, familyName } = payload
+
+    if (isNil(email) || email === '') {
+      throw new BadRequestException('Can not get email address')
+    }
+
+    const user = await this.userService.firstOrCreate(
+      { where: email },
+      {
+        email: this.userService.sanitizeEmail(email),
+        firstName: familyName,
+        lastName: givenName,
+      },
+    )
 
     const partialUserProperties: Partial<UserEntity> = pick(
       user,
